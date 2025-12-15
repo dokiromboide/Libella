1e2791fe3acfe80048b0fc06e9a24528ae96a203
Dokiromboide <jesuscaicedo800@gmail.com>
2025-12-15 15:38:09 -0500

    Elimina imports duplicados en AppRouter.tsx

--- src/paginas/AppRouter.tsx
+++ src/paginas/AppRouter.tsx
@@ -6,11 +6,9 @@ import TodasNoticias from "./noticias/TodasNoticias";
 import TodosProyectos from "./proyectos/TodosProyectos";
 import Inversiones from "./Inversiones";
 import Servicios from "./Servicios";
-import Servicios from "./Servicios";
 import Contactanos from "./Contactanos";
 import Nosotros from "./Nosotros";
 import Header from "../componentes/Header";
-import { proyectosData } from "../datos/proyectos";
 import { proyectosData } from "../datos/proyectos";
 
 // Componente para la página principal (Inicio)
